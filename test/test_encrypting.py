4f4a5104b476fc20166a96aae4fbbe62c80e23f4
kaixa <690750353@qq.com>
2020-04-03 14:23:33 +0800

    不再上传测试文件

--- test/test_encrypting.py
+++
@@ -1,5 +0,0 @@
-import util.Encrypting as E
-
-
-def test_md5():
-    assert len(E.Encryptor.md5('LABxhjj1802@#')) == 32
--- test/test_util.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def test_plus_tuple():
-    t1 = (1, 2)
-    t2 = ('3', [4, 5])
-    t3 = t1 + t2
-    assert t3 == (1, 2, '3', [4, 5])
-
