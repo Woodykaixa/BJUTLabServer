c53631c78291652d5ceb08085e90b65504c8c236
kaixa <690750353@qq.com>
2020-04-14 11:26:17 +0800

    增加RepoPushed的发送方检查

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -3,8 +3,10 @@ import re
 from signal import SIGKILL
 from time import sleep
 
+
 out = os.popen('pstree -ap|grep gunicorn').read().split('\n')[0]
 pid = re.findall(r'\d+', out)[0]
 os.kill(int(pid), SIGKILL)
+os.system('git clone https://github.com/Woodykaixa/BJUTLabServer.git')
 sleep(2)
 os.system('cd /usr/BJUTLabServer && source venv/bin/activate && gunicorn "BJUTLabServer:create_app()"')
--- BJUTLabServer/__init__.py
+++ BJUTLabServer/__init__.py
@@ -1,5 +1,7 @@
 def create_app():
     from flask import Flask, request
+    import json
+    import os
     from flask_cors import CORS
     from .exception import ParameterException, WerkzeugException
     from .utilities import make_error_response
@@ -20,8 +22,12 @@ def create_app():
 
     @app.route('/RepoPushed', methods=['POST'])
     def update_self():
-        app.logger.info(str(request.headers))
-        app.logger.info(str(request.json))
+        event = request.headers['X-GitHub-Event']
+        agent = request.headers['User-Agent']
+        body = json.loads(request.json)
+        repo_id = body['repository']['id']  # 252450181
+        if event == 'push' and agent == 'GitHub-Hookshot/1f3832a' and repo_id == 252450181:
+            os.system('git fetch && rm -rf test/')
         return ""
 
     @app.errorhandler(ParameterException)
