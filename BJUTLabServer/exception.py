72cb23921d8acdae980d42f3603de19b75a2d85a
kaixa <690750353@qq.com>
2020-04-20 21:45:39 +0800

    增加新的异常类InvalidParameter用来替代ParameterException

--- BJUTLabServer/exception.py
+++ BJUTLabServer/exception.py
@@ -11,7 +11,20 @@ class APIReinitializationError(Exception):
 class ParameterException(WerkzeugException.HTTPException):
     """
     当调用api时提供的参数与api的参数不同时，则抛出此异常
+    DEPRECATED!! - 为了返回信息的统一，不推荐使用此异常，
+    应使用``InvalidParameter``替代
     """
+
     def __init__(self, code, desc):
         ParameterException.code = code
         ParameterException.description = desc
+
+
+class InvalidParameter(ParameterException):
+    """
+    当调用API时参数不符合规定时，抛出此异常。
+    """
+
+    def __init__(self, code, desc):
+        InvalidParameter.code = code
+        InvalidParameter.description = 'Invalid parameter: {}'.format(desc)
