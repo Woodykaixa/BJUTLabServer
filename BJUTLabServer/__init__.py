4e0771dc439a517705358304be5b259fe5e44be4
kaixa <690750353@qq.com>
2020-05-15 16:36:27 +0800

    移除了用不到的文件，index页面显示所有可用api
    Signed-off-by: kaixa <690750353@qq.com>

--- BJUTLabServer/__init__.py
+++ BJUTLabServer/__init__.py
@@ -1,14 +1,13 @@
-STARTUP_TIME = None
-
-
 def create_app():
     from flask import Flask, render_template
     from flask_cors import CORS
     from .exception import ParameterException, WerkzeugException
     from .utilities import make_error_response
     from .blueprints import BPList
+    from .utilities.Log import Log
     from datetime import datetime
 
+    VERSION_CODE = '0.1.0'
     STARTUP_TIME = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     app = Flask('BJUTLabServer')
@@ -18,10 +17,20 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY='afkJLSLjfljkKLS8Rsfj234LMNK'
     )
+    rules = app.url_map.iter_rules()
+
+    APIs = {}
+    for rule in rules:
+        pair = str(rule).split('/', 2)
+        if pair[1] in APIs:
+            if pair[2] not in APIs[pair[1]]:
+                APIs[pair[1]].append(pair[2])
+        else:
+            APIs[pair[1]] = [pair[2]]
 
     @app.route('/')
     def hi():
-        return render_template('index.html', time=STARTUP_TIME)
+        return render_template('index.html', time=STARTUP_TIME, APIs=APIs, ver=VERSION_CODE)
 
     @app.errorhandler(ParameterException)
     def handle_parameter_exception(e):
